fe8a0e0a40340a2275519e891418310be4071369
Carlos Gomez <cgomezm@pucp.pe>
2025-02-04 15:45:23 -0500

    feat: tarea

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:numeros_whats_app/core/screens/detalles_numero_de_telefono.dart';
-import 'package:numeros_whats_app/core/screens/pagina_principal.dart';
-import 'package:numeros_whats_app/routes/routes.dart';
+import 'package:numeros_whats_app/display/screens/detalles_numero_de_telefono.dart';
+import 'package:numeros_whats_app/display/screens/pagina_principal.dart';
+import 'package:numeros_whats_app/display/routes/routes.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
